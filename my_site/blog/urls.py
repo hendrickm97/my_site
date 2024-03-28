55da70f50ee514d983957f49a6631f60d09574b9
Hendrick Menacho <hmenacho97@gmail.com>
2024-03-28 09:43:16 -0300

    corrigindo erros

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from my_site.blog import views
+from blog import views
 
 urlpatterns = [
     path('', views.PostView.as_view(), name='home')
--- my_site/mysite/urls.py
+++ my_site/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for my_site project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from my_site.blog import urls
+from blog import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
